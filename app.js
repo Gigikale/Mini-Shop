b2ae058f6cfb121cb2bcc212133e209da734df62
“Gigikale” <luckygloria93@gmail.com>
2024-09-20 14:44:23 +0100

    fix

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const  sequelize  = require('./config/database')
-const route  = require('./routes/allRoutes')
-const logger = require('morgan')
-const app = express()
-const dotenv = require("dotenv")
+const sequelize = require('./config/database');
+const route = require('./routes/allRoutes');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
+
+const app = express();
+
+
+app.use(cors()); 
 app.use(bodyParser.json());
-app.use(logger("dev"));
-app.use('/api', route)
+app.use(logger('dev'));
+
+
+app.use('/api', route);
 
 
 sequelize.sync({})
-.then(() => {
-  console.log("Database is connected");
-})
-.catch((err) => {
-  console.log(err);
+  .then(() => {
+    console.log('Database is connected');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-app.listen(3000, ()=>{
-  console.log("App running on port 3000")
-})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
